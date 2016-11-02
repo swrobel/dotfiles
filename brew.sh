52fc69a548913fe46da6e273738f752f240fdad8
Stefan Wrobel <swrobel@users.noreply.github.com>
2016-11-02 15:57:29 -0700

    [brew] remove Sierra-incompatible formulae, others that are no longer used, add MS fonts

--- brew.sh
+++ brew.sh
@@ -11,7 +11,6 @@ brew install mailhog
 brew install openssl
 brew install python3
 brew install ruby-build
-brew install speedtest_cli
 brew install terminal-notifier
 brew install trash
 brew install rmtrash
@@ -19,8 +18,6 @@ brew install wget --with-iri
 brew install zsh
 
 brew tap caskroom/cask
-brew install brew-cask
-
 brew cask install appcleaner
 brew cask install atom
 brew cask install bartender
@@ -31,25 +28,23 @@ brew cask install flux
 brew cask install google-chrome
 brew cask install istat-menus
 brew cask install kaleidoscope
-brew cask install karabiner
 brew cask install lastpass
 brew cask install libreoffice
 brew cask install quip
-brew cask install radiant-player
 brew cask install recordit
-brew cask install seil
 brew cask install skype
 brew cask install sourcetree
 brew cask install spotify
 brew cask install spotify-notifications
-brew cask install spotifree
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install vivaldi
 brew cask install vlc
-brew cask install xtrafinder
 
 brew tap caskroom/fonts
 brew cask install font-hack
+
+brew tap colindean/fonts-nonfree
+brew cask install font-microsoft-office
 
 echo "Manually install Typinator 5.9 & Dropbox"
