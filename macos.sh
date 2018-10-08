8db118837fd3e55dff8016c530602524c43902bb
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-10-08 10:32:18 -0700

    [macOS] make key repeat delay/rate a little slower

--- macos.sh
+++ macos.sh
@@ -116,10 +116,10 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Remove delay before keyboard repeat
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -float 1.2
+defaults write NSGlobalDomain KeyRepeat -float 1.5
 
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
