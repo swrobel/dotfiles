62e3ffc97b82997f652adee4f11a72d01f665c54
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-05-14 14:14:00 -0700

    [brew] update to latest homebrew formulae

--- brew.sh
+++ brew.sh
@@ -4,12 +4,10 @@ brew install coreutils
 brew install diff-so-fancy
 brew install exiftool
 brew install fd
-brew install ffmpeg
 brew install fzf
 brew install git
 brew install highlight
 brew install htop
-brew install hub
 brew install lesspipe
 brew install mackup
 brew install mailhog
@@ -28,37 +26,30 @@ brew install wget --with-iri
 brew install yarn
 brew install zsh
 
-brew tap caskroom/cask
+brew cask install 1password
 brew cask install appcleaner
 brew cask install atom
 brew cask install bartender
-brew cask install encryptme
+brew cask install bettertouchtool
+brew cask install dropbox
 brew cask install firefox
-brew cask install fluid
 brew cask install flycut
 brew cask install google-chrome
 brew cask install iina
 brew cask install istat-menus
 brew cask install kaleidoscope
-brew cask install lastpass
 brew cask install libreoffice
 brew cask install mediainfo
 brew cask install macmediakeyforwarder
-brew cask install nightowl
 brew cask install postico
 brew cask install quip
-brew cask install recordit
 brew cask install skype
 brew cask install slack
+brew cask install sourcetree
 brew cask install spotify
 brew cask install spotify-notifications
-brew cask install sublime-merge
 brew cask install sublime-text
-brew cask install vivaldi
 brew cask install whatsapp
-
-brew tap caskroom/fonts
-brew cask install font-hack
 
 brew tap colindean/fonts-nonfree
 brew cask install font-microsoft-office
@@ -66,6 +57,9 @@ brew cask install font-microsoft-office
 brew install heroku/brew/heroku
 brew install TomAnthony/brews/itermocil
 
-mv /Applications/Spotify\ Notifications.app /Applications/SNotifications.app
+brew tap federico-terzi/espanso
+brew install espanso
 
-echo "Manually install BetterTouchTool 2.48, Typinator 5.9 & Dropbox"
+brew install github/gh/gh
+
+mv /Applications/Spotify\ Notifications.app /Applications/SNotifications.app
