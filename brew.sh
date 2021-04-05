6eefdf2b767fc891454e7381978264a31349cef6
System Administrator <root@LC02CH2BJMD6R.US.AD.WELLPOINT.COM>
2021-04-05 13:29:30 -0700

    [brew] Use install --cask instead of old cask install

--- brew.sh
+++ brew.sh
@@ -26,38 +26,35 @@ brew install wget
 brew install yarn
 brew install zsh
 
-brew cask install 1password
-brew cask install appcleaner
-brew cask install bartender
-brew cask install bettertouchtool
-brew cask install dropbox
-brew cask install firefox
-brew cask install flycut
-brew cask install google-chrome
-brew cask install iina
-brew cask install istat-menus
-brew cask install iterm2
-brew cask install kaleidoscope
-brew cask install libreoffice
-brew cask install mediainfo
-brew cask install macmediakeyforwarder
-brew cask install postico
-brew cask install quip
-brew cask install skype
-brew cask install slack
-brew cask install sourcetree
-brew cask install spotify
-brew cask install spotify-notifications
-brew cask install sublime-text
-brew cask install whatsapp
+brew install --cask 1password
+brew install --cask appcleaner
+brew install --cask bartender
+brew install --cask bettertouchtool
+brew install --cask dropbox
+brew install --cask firefox
+brew install --cask flycut
+brew install --cask google-chrome
+brew install --cask iina
+brew install --cask istat-menus
+brew install --cask iterm2
+brew install --cask kaleidoscope
+brew install --cask libreoffice
+brew install --cask mediainfo
+brew install --cask macmediakeyforwarder
+brew install --cask postico
+brew install --cask quip
+brew install --cask skype
+brew install --cask slack
+brew install --cask sourcetree
+brew install --cask spotify
+brew install --cask sublime-text
+brew install --cask whatsapp
 
 brew tap colindean/fonts-nonfree
-brew cask install font-microsoft-office
+brew install --cask font-microsoft-office
 
 brew install heroku/brew/heroku
 brew install TomAnthony/brews/itermocil
 
 brew tap federico-terzi/espanso
 brew install espanso
-
-mv /Applications/Spotify\ Notifications.app /Applications/SNotifications.app
